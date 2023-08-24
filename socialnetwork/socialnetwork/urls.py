d14d8f7047f68bbe507d845bbdd6b87a75216998
argen005 <137420237+argen005@users.noreply.github.com>
2023-08-24 21:06:00 +0600

    social network

--- socialnetwork/socialnetwork/urls.py
+++ socialnetwork/socialnetwork/urls.py
@@ -2,10 +2,12 @@ from django.contrib import admin
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from chat.views import ChatViewSet, MessageViewSet
+from users.views import ProfileViewSet
 
 router = DefaultRouter()
 router.register(r'chats', ChatViewSet)
 router.register(r'messages', MessageViewSet)
+router.register(r'users', ProfileViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
--- socialnetwork/users/models.py
+++ socialnetwork/users/models.py
@@ -1,8 +1,7 @@
 from django.db import models
-from django.contrib.auth.models import User
 
 class Profile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    user = models.CharField(max_length=20)
     name = models.CharField(max_length=100)
     gender = models.CharField(max_length=10)
     age = models.PositiveIntegerField()
